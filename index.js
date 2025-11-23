bb32aeab2e5c965f3a677ce7be9b7e1958cd66c2
Ori <buhbutoriasher@gmail.com>
2025-11-23 08:34:29 +0200

    add all levels

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const _ = require('lodash');
 const appsettings = JSON.parse(fs.readFileSync('./appsettings.json').toString());
 const RESOURCES = JSON.parse(fs.readFileSync('./resources.json'));
 const EBX_PATH = appsettings.EbxPath;
-const MAIN_FOLDER_PATH = appsettings.mainFolder;
-const ASSET_NAME = appsettings.assetName;
 const GUID_REGEX = /[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}/;
 const PATH_BEFORE_GUID_REGEX = /\/[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}/;
 const resources = [];
@@ -34,102 +32,67 @@ function GetReferencedFileNames(file) {
 
 function SaveFiledata(filePath, data) {
     SearchPartitions(filePath, data);
-    SearchChunks(filePath, data);
     SearchResources(filePath, data);
+    //SearchChunks(filePath, data);
 }
 
 function SearchPartitions(filePath, fileData) {
-    const partition = fileData.toString()
-        .split('\n').join('')
-        .split('\r')
-        .filter(row => row.includes("Partition "))[0];
-
-    partitions.push(partition.split("Partition ")[1]);
+    let splitted = filePath.split('.txt');
+    splitted = splitted[0];
+    partitions.push(splitted);
 }
 
 function SearchResources(filePath, fileData) {
-    console.log(`Searching Resources --> ${filePath}`);
-    const bar = new ProgressBar(':bar :percent', { total: RESOURCES.length, width: 40 });
     for (let i = 0; i < RESOURCES.length; i++) {
-        bar.tick();
         if (fileData.toLowerCase().indexOf(RESOURCES[i].Name.toLowerCase()) > -1) {
             resources.push({
                 resourceName: RESOURCES[i].Name,
                 resourceType: RESOURCES[i].TypeName,
             });
-            bar.tick(bar.total - bar.curr);
         }
     }
 }
 
-function SearchChunks(filePath, fileData) {
-    console.log(`Searching Chunks --> ${filePath}`);
-    const rows = fileData.toString()
-        .split(/\r?\n/).join('')
-        .split('\t')
-        .filter(row => GUID_REGEX.test(row));
+// function SearchChunks(filePath, fileData) {
+//     const rows = fileData.toString()
+//         .split(/\r?\n/).join('')
+//         .split('\t')
+//         .filter(row => GUID_REGEX.test(row));
 
-    const bar = new ProgressBar(':bar :percent', { total: rows.length, width: 40 });
-    for (let i = 0; i < rows.length; i++) {
-        bar.tick();
-        if (rows[i].includes("ChunkId")) {
-            const chunkId = rows[i].split(' ')[1];
-            if (chunks.findIndex(ch => ch == chunkId) == -1) {
-                chunks.push(chunkId);
+//     for (let i = 0; i < rows.length; i++) {
+//         if (rows[i].includes("ChunkId")) {
+//             const chunkId = rows[i].split(' ')[1];
+//             if (chunks.findIndex(ch => ch == chunkId) == -1) {
+//                 chunks.push(chunkId);
+//             }
+//         }
+//     }
+// }
+
+
+const bar = new ProgressBar(':bar :percent', { total: appsettings.assets.length, width: 40 });
+for (const asset of appsettings.assets) {
+    bar.tick()
+    var mainFile = fs.readFileSync(path.join(EBX_PATH, asset));
+    SaveFiledata(path.join(asset), mainFile.toString());
+
+    var fileRows = GetReferencedFileNames(mainFile);
+    var savedPaths = []
+    while (fileRows.length > 0) {
+        for (const row in fileRows) {
+            let file = fs.readFileSync(path.join(EBX_PATH, fileRows[row]));
+            if (fileRows[row].includes("Materials/MaterialContainer") == false) {
+                SaveFiledata(fileRows[row], file.toString());
+                savedPaths.push(...GetReferencedFileNames(file));
             }
         }
+        fileRows = [];
+        fileRows = JSON.parse(JSON.stringify([...new Set(savedPaths)]));
+        savedPaths = [];
     }
 }
 
-async function GenerateRimeCommandFile(type) {
-    //TODO: Implements
-    switch (type) {
-        case "get-meshset-chunk-ids":
-            break;
-        case "dump-all":
-            break;
-        case "build-new-bundle":
-            break;
-    }
-}
-
-function GetMeshSetChunks() {
-    //TODO: Implement
-}
-
-
-function DumpAll() {
-    //TODO: Implement
-}
-
-function BuildNewBundle() {
-    //TODO: Implement
-}
-
-async function RunRimeBuilder() {
-    //TODO: Implement
-}
-
-var mainFile = fs.readFileSync(EBX_PATH + MAIN_FOLDER_PATH + ASSET_NAME + '.txt');
-SaveFiledata(path.join(ASSET_NAME, MAIN_FOLDER_PATH + ASSET_NAME + '.txt'), mainFile.toString());
-
-var fileRows = GetReferencedFileNames(mainFile);
-
-var savedPaths = []
-while (fileRows.length > 0) {
-    for (const row in fileRows) {
-        let file = fs.readFileSync(path.join(EBX_PATH, fileRows[row]));
-        SaveFiledata(path.join(ASSET_NAME, fileRows[row]), file.toString());
-        savedPaths.push(...GetReferencedFileNames(file));
-    }
-    fileRows = [];
-    fileRows = JSON.parse(JSON.stringify([...new Set(savedPaths)]));
-    savedPaths = [];
-}
-
-(
-    async () => {
-        console.log('RunRimeBuilder');
-        await RunRimeBuilder();
-    }
-)
+fs.mkdirSync('./output', { recursive: true });
+fs.writeFileSync('./output/partitions.json', JSON.stringify(partitions, null, 2));
+fs.writeFileSync('./output/chunks.json', JSON.stringify(chunks, null, 2));
+fs.writeFileSync('./output/resources.json', JSON.stringify(resources, null, 2));
